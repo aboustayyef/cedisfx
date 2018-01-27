848d5485571ea489f305553c82d8f8c1285f0a93
Mustapha Hamoui <mustapha.hamoui@gmail.com>
2018-01-27 11:09:22 +0000

    preparing for mailing

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        //
+        Commands\RateChecker::class,
     ];
 
     /**
@@ -24,8 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')
-        //          ->hourly();
+        $schedule->command('app:check_rate')->EveryFiveMinutes();
     }
 
     /**
